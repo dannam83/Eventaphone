62342823f5651a914ebf1e6f72a77b7e7d277afa
Daniel Nam <dannam83@gmail.com>
2018-01-31 01:35:18 -0500

    add mechanize to syndicate events to other websites

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,6 +15,8 @@
 #  syndicated :boolean
 #
 
+# require 'mechanize'
+
 class Event < ApplicationRecord
   validates :name, :date, :time, :address, presence: true
   validates :summary, :details, :organizer, presence: true
@@ -33,20 +35,41 @@ class Event < ApplicationRecord
   def self.syndicate
     queued_events = Event.where(syndicated: nil)
     queued_events.each do |event|
-
+      event.syndicate
     end
   end
 
   def syndicate
-    self.fill_form("eventbrite")
-    self.fill_form("timeout")
-    self.fill_form("nyc")
-    self.fill_form("events12")
-    self.fill_form("thrillist")
+    self.fill_form("eventbrite.com")
+    self.fill_form("lanyrd.com")
+    self.fill_form("yelp.com")
+    self.fill_form("eventful.com")
+    self.fill_form("eventsetter.com")
     self.syndicated = true
+    self.save
   end
 
-  def fill_form(website)
+  def fill_form(url)
+    agent = Mechanize.new
+    page = agent.get("http://" + url)
+    agent.page.forms[0]["name"] = self.name
+    agent.page.forms[0]["title"] = self.name
+    agent.page.forms[0]["Time"] = self.time
+    agent.page.forms[0]["date"] = self.date
+    agent.page.forms[0]["address"] = self.address
+    agent.page.forms[0]["location"] = self.address
+    agent.page.forms[0]["summary"] = self.summary
+    agent.page.forms[0]["details"] = self.details
+    agent.page.forms[0]["description"] = self.details
+    agent.page.forms[0]["organizer"] = self.organizer
+    agent.page.forms[0]["event[name]"] = self.name
+    agent.page.forms[0]["event[time]"] = self.time
+    agent.page.forms[0]["event[date]"] = self.date
+    agent.page.forms[0]["event[address]"] = self.address
+    agent.page.forms[0]["event[summary]"] = self.summary
+    agent.page.forms[0]["event[details]"] = self.details
+    agent.page.forms[0]["event[organizer]"] = self.organizer
+    page.forms[0].submit
   end
 
 end
