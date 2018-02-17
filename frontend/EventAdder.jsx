93d23f2e681b71c5bbee466b00c6689767f55a38
Daniel Nam <dannam83@gmail.com>
2018-02-17 16:56:30 -0500

    add event listener for submit

--- frontend/EventAdder.jsx
+++ frontend/EventAdder.jsx
@@ -1,6 +1,17 @@
-var EventAdder = function($el) {
-  this.$form = $el.find("form");
-  this.$events = $el.find("ul");
-};
+class EventAdder {
+  constructor($el){
+    this.$form = $el.find("form");
+    this.$events = $el.find("ul");
+
+    this.$form.on("submit", this.submitMessage.bind(this));
+  }
+
+  submitMessage(e){
+    e.preventDefault();
+    alert("nothing but the alert");
+  }
+
+}
+
 
 module.exports = EventAdder;
