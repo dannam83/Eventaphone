6569b09f035a64e27fc4d12068e2409753dc7656
Daniel Nam <dannam83@gmail.com>
2018-02-17 15:44:25 -0500

    fix jquery issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   root to: 'static_pages#root'
-  
+
   namespace :api, defaults: { format: :json } do
-    resources :events, only: [:show, :create, :update, :destroy]
+    resources :events, only: [:show, :create, :destroy]
   end
 
 end
