0729bc81a1134d59cdcb1a986bd040b0af85cf38
Daniel Nam <dannam83@gmail.com>
2018-02-18 02:18:53 -0500

    update frontend to update without page without refresh using jquery

--- frontend/EventAdder.jsx
+++ frontend/EventAdder.jsx
@@ -3,23 +3,37 @@ class EventAdder {
     this.$form = $el.find("form");
     this.$events = $el.find("ul");
 
-    this.$form.on("submit", this.submitMessage.bind(this));
+    this.$form.on("submit", this.submitEvent.bind(this));
   }
 
-  submitMessage(e){
+  submitEvent(e){
     e.preventDefault();
     $.ajax({
       method: "post",
       url: "/api/events",
       dataType: "json",
       data: this.$form.serialize(),
-      success: function(){
-        alert("hooray it worked");
-      },
+      success: function(event){
+        this.addEvent(event);
+        this.clearForm();
+      }.bind(this),
       error: function(){
         alert("Error. Not able to save event.");
       }
     });
+  }
+
+  addEvent(event){
+    $(".noEventsMarker").remove();
+    const $event = $("<li></li>").text(
+      `${event.name}: ${event.summary}`
+    );
+    this.$events.append($event);
+  }
+
+  clearForm(){
+    this.$form.find("input[type='text']").val("");
+    this.$form.find("textarea").val("");
   }
 
 }
